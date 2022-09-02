9ea5da0bf22d790a58c220144baf542fb5c1790e
Olesya Solovyova <soe611@yandex.ru>
2022-09-02 19:29:29 +0300

    Hometask, max of 3 numbers

---
+++ Sem1Task4 HW/Program.cs
@@ -0,0 +1,37 @@
+﻿//====================================================
+// Напишите программу, которая принимает на вход три 
+// числа и выдаёт максимальное из этих чисел.
+//  2, 3, 7 -> 7
+//  44 5 78 -> 78
+//  22 3 9 -> 22
+//====================================================
+
+//Console.Clear();
+
+Console.Write("Помогу узнать максимум из трёх чисел!:) \nВведите первое число: ");
+string inputLineA = Console.ReadLine() ?? "";
+Console.Write("Введите второе число: ");
+string inputLineB = Console.ReadLine() ?? "";
+Console.Write("Введите третье число: ");
+string inputLineC = Console.ReadLine() ?? "";
+
+int inputNumberA = int.Parse(inputLineA);
+int inputNumberB = int.Parse(inputLineB);
+int inputNumberC = int.Parse(inputLineC);
+int maxNumber;
+
+if (inputNumberA > inputNumberB)
+{
+    maxNumber = inputNumberA;
+}
+else
+{
+    maxNumber = inputNumberB;
+}
+
+if (inputNumberC > maxNumber)
+{
+    maxNumber = inputNumberC;
+}
+
+Console.WriteLine("Max = " + maxNumber);
