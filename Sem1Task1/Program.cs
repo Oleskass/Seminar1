ee7741c701341e37064418316a9909de45ba7753
Olesya Solovyova <soe611@yandex.ru>
2022-09-02 13:23:45 +0300

    create folder for Task2

---
+++ Sem1Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿//====================================================
+// #0 Напишите программу, которая на вход принимает
+// число и выдаёт его квадрат (число умноженное на
+// само себя).
+// Например: 4 -> 16, -3 -> 9, -7 -> 49
+//====================================================
+
+string? inputLine = Console.ReadLine();
+if (inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+    //int outputNumber = inputNumber * inputNumber;
+    int outputNumber = (int)Math.Pow(inputNumber,2);
+
+    Console.WriteLine(outputNumber);
+}
