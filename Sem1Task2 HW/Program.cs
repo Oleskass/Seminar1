2c48c0316038d1f3294263a38d17e114847c9a0c
Olesya Solovyova <soe611@yandex.ru>
2022-09-02 18:33:06 +0300

    Hometask, max and min of 2 numbers

---
+++ Sem1Task2 HW/Program.cs
@@ -0,0 +1,26 @@
+﻿//====================================================
+// #2 Напишите программу, которая на вход принимает 
+// два числа и выдаёт, какое число большее, а какое меньшее.
+//  a = 5; b = 7 -> max = 7
+//  a = 2 b = 10 -> max = 10
+//  a = -9 b = -3 -> max = -3
+//====================================================
+
+Console.Clear();
+
+Console.Write("Помогу узнать максимум и минимум из двух чисел!:) \nВведите первое число: ");
+string inputLineA = Console.ReadLine() ?? "";
+Console.Write("Введите второе число: ");
+string inputLineB = Console.ReadLine() ?? "";
+
+int inputNumberA = int.Parse(inputLineA);
+int inputNumberB = int.Parse(inputLineB);
+
+if (inputNumberA > inputNumberB)
+{
+    Console.WriteLine("Max = " + inputNumberA + ", min = " + inputNumberB);
+}
+else
+{
+    Console.WriteLine("Max = " + inputNumberB + ", min = " + inputNumberA);
+}
