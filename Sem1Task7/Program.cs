5248a745a732b27ef702f39d3713d53474881148
Olesya Solovyova <soe611@yandex.ru>
2022-09-02 18:05:22 +0300

    take 3-digit number and show us last digit of it

---
+++ Sem1Task7/Program.cs
@@ -0,0 +1,20 @@
+﻿//====================================================
+// #7 Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает последнюю 
+// цифру этого числа. Например:
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+//====================================================
+
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+    if (inputNumber > 99 && inputNumber < 1000)
+    {
+        int lastDigit = inputNumber % 10;
+        Console.WriteLine(lastDigit);
+    }
+}
