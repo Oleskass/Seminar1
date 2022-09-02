e7c893ef9fc979664050e9d24b72a05ad145d4de
Olesya Solovyova <soe611@yandex.ru>
2022-09-02 14:06:30 +0300

    bool - if numA is square of numB

--- Sem1Task1/Program.cs
+++ Sem1Task1/Program.cs
@@ -1,16 +1,40 @@
 ﻿//====================================================
-// #0 Напишите программу, которая на вход принимает
-// число и выдаёт его квадрат (число умноженное на
-// само себя).
-// Например: 4 -> 16, -3 -> 9, -7 -> 49
+// #1 Напишите программу, которая на вход принимает
+// два числа и проверяет, является ли первое число
+// квадратом второго.
+// Например:
+// a = 25; b = 5 -> да
+// a = 2 b = 10 -> нет
+// a = 9; b = -3 -> да
+// a = -3 b = 9 -> нет
 //====================================================
 
-string? inputLine = Console.ReadLine();
-if (inputLine != null)
+string? inputLineA = Console.ReadLine();
+string? inputLineB = Console.ReadLine();
+if (inputLineA != null && inputLineB != null)
 {
-    int inputNumber = int.Parse(inputLine);
-    //int outputNumber = inputNumber * inputNumber;
-    int outputNumber = (int)Math.Pow(inputNumber,2);
+    int inputNumberA = int.Parse(inputLineA);
+    int inputNumberB = int.Parse(inputLineB);
 
-    Console.WriteLine(outputNumber);
+    bool outResult = (inputNumberA == inputNumberB * inputNumberB);
+
+    //bool outResult = (inputNumberB == inputNumberA / inputNumberB);
+    //bool outResult = (Math.Sqrt(inputNumberA) == inputNumberB);
+    //bool outResult = (inputNumberA == (int)Math.Pow(inputNumberB,2));
+
+    Console.WriteLine(outResult);
 }
+
+// string inputLineA = Console.ReadLine()??"";
+// string inputLineB = Console.ReadLine()??"";
+
+//     int inputNumberA = int.Parse(inputLineA);
+//     int inputNumberB = int.Parse(inputLineB);
+
+//     bool outResult = (inputNumberA == inputNumberB * inputNumberB);
+
+//     //bool outResult = (inputNumberB == inputNumberA / inputNumberB);
+//     //bool outResult = (Math.Sqrt(inputNumberA) == inputNumberB);
+//     //bool outResult = (inputNumberA == (int)Math.Pow(inputNumberB,2));
+
+//     Console.WriteLine(outResult);
